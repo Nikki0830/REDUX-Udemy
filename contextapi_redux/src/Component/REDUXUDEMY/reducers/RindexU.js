21799a1dcd4204250ffd5e10201edbfefcd774d9
Nikki0830 <gniki0830@gmail.com>
2024-01-30 15:55:21 +0530

    REDUX_UDEMY

--- contextapi_redux/src/Component/REDUXUDEMY/reducers/RindexU.js
+++ contextapi_redux/src/Component/REDUXUDEMY/reducers/RindexU.js
@@ -8,12 +8,49 @@ const RindexU = () => {
   }
   let message = greeting;
 
-  function printMessage(fun) {
-    console.log(fun);
-  }
-  printMessage(message());
+  // function printMessage(fun) {
+  //   console.log(fun);
+  // }
+  // printMessage(message());
   // printMessage(greeting());
+  // function printMessage(fun){
+  //   console.log(message())
+  // }
+  // printMessage()
+  function printMessage(fun) {
+    console.log(fun());
+  }
+  printMessage(message);
 
+  function returnFun() {
+    return function () {
+      return "Good Morning😊";
+    };
+  }
+
+  let funvar = returnFun();
+  let msg = funvar();
+  console.log(msg);
+
+  //higher order function
+
+  let arr = [1, 2, 4, 6, 7];
+  const data = arr.map((x) => x * 2);
+  console.log(data);
+
+  //function compposition
+  let msg1 = " Nikki ";
+  let msg2 = "Hello " + msg1.trim() + " Good morning";
+
+  //in function programming
+  let trimMsg = (name) => name.trim();
+  // let pMsg = (name) => "Hello " + name + "gm";
+  let pMsg = (name) => `Hello ${name} GOOD MORNING`;
+  const result = pMsg(trimMsg(msg1));
+  // const result = pMsg(msg1);
+  console.log(result);
+
+  console.log(msg2);
   return (
     <div>
       <h1>REDUX UDEMY {message()}</h1>
