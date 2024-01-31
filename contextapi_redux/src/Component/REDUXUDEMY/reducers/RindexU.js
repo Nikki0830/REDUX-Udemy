f6af7ec35c62a845cc1da682b66798f0fa68c219
Nikki0830 <gniki0830@gmail.com>
2024-01-31 17:55:40 +0530

    REDUX_UDEMY

--- contextapi_redux/src/Component/REDUXUDEMY/reducers/RindexU.js
+++ contextapi_redux/src/Component/REDUXUDEMY/reducers/RindexU.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { compose, pipe } from "lodash/fp";
+// import { compose, pipe } from "lodash/fp";
+import { pipe } from "lodash/fp";
+import { produce } from "immer";
 
 const RindexU = () => {
   // console.log("resux udemy");
@@ -103,6 +105,94 @@ const RindexU = () => {
   const newValue1 = pipe(trimMsg1, convertToUper2, pMsg3("good morning"));
   const result1 = newValue1(msg1);
   console.log(result1);
+
+  //Imutability in objects with Object.assign()
+
+  const user = { name: "Nikki", age: 25 };
+  const newUser = Object.assign({}, user, { name: "Nikita" });
+  console.log(user);
+  console.log(newUser);
+
+  //immutabily in objects with spread operator
+  const user2 = { name: "Nikita Patel", age: 25 };
+  const newUser2 = {
+    ...user2, //it will copy all propersties of user
+    name: "NIKKI PATEL",
+  };
+  console.log(user2);
+  console.log(newUser2);
+
+  //addd one more obj which is an object
+  const user3 = {
+    name: "Nikita Patel",
+    age: 25,
+    company: { country: "INDIA", city: "Bangalore" },
+  };
+  // const newUser3 = {
+  //   ...user3,
+  //   name: "NIKKI NIKKI",
+  //   //udpate company obj here
+  //   company: { ...user3.company, city: "Berlin" },
+  // };//same thing with immmer(to deal immutability of objects and arrrays)
+  const newuser4 = produce(user3, (draftState) => {
+    draftState.name = "REEMA";
+    draftState.company.city = "BERLIN";
+  });
+
+  // newUser3.company.city = "Berlin"; //by dpoing this we get
+  //value in both the objc because user3 and
+  //newuser3 are both in same address
+  console.log(user3);
+  console.log(newuser4);
+
+  //immutability with arrays
+  const num = [1, 2, 3, 4, 5];
+  //change value in a specific place
+  const index = num.indexOf(4);
+  const newNum = [...num.slice(0, index), 50, ...num.slice(index)];
+  // const newNum = [50, ...num]; //[...num,50]
+  console.log(num);
+  console.log(newNum);
+
+  //update the array by map()
+  const newv = num.map((x) => (x === 4 ? 50 : x));
+  console.log(num);
+  console.log(newv);
+
+  //remove item from array with filter()
+  const filterv = num.filter((x) => x !== 4);
+  console.log(num);
+  console.log(filterv);
+
+  const book = {
+    author: "xxxxx",
+    book2: {
+      name: "ggg",
+      price: "67",
+      rating: 4.5,
+    },
+  };
+
+  // const newbook = {
+  //   ...book,
+  //   author:"ttttt",
+  //   book2:{...book.book2,name:"444"}
+  // }
+  const newbook = produce(book, (draftState) => {
+    draftState.book2.name = "77777";
+    draftState.book2.rating = 5;
+  });
+  // newbook.book2.name="6666"
+  console.log(book);
+  console.log(newbook);
+
+  const book2 = ["b1", "b2", "b3"];
+  //const index2 = book2.indexOf("b2")
+  // const book3 = [...book2.slice(0,1),"b4",...book2.slice(index2)]
+  // const book3 = book2.map((x) => (x === "b2" ? "b4" : x));
+  const book3 = book2.filter((x) => (x !== "b2"));
+  console.log(book2);
+  console.log(book3);
 
   return (
     <div>
